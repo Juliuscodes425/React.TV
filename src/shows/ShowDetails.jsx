eaf00db7aa2df0d06a9c120ff7f522f6e7e9a50e
Dylan Jules <dylan.jules@oakwood.edu>
2025-06-09 00:56:16 -0400

    react TV

--- src/shows/ShowDetails.jsx
+++ src/shows/ShowDetails.jsx
@@ -1,6 +1,20 @@
 import "./shows.css";
+import "./EpisodeList";
+import "./ShowSelection";
+import "./EpisodeList";
+import React, { useState } from "react";
 
 /** Allows users to browse through the episodes of the given show */
-export default function ShowDetails() {
+export default function ShowDetails({ tvShows }) {
+  const [selectedEpisode, setSelectedEpisode] = useState(null);
   return <div className="show-details"></div>;
 }
+if (!tvShows) {
+  return <div>select a show</div>;
+}
+return (
+  <div className="show-details">
+    <EpisodeList episode={selectedEpisode} />
+    <EpisodeDetails />
+  </div>
+);
